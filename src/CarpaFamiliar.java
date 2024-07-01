fe4f4cdaad6a77638468093ee8c2d6f079c3253d
AgustinLuporini <153564780+AgustinLuporini@users.noreply.github.com>
2024-07-01 17:36:37 -0300

    Delete src directory

--- src/CarpaFamiliar.java
+++
@@ -1,5 +0,0 @@
-public class CarpaFamiliar extends Carpa{
-    public CarpaFamiliar(int precio, int capacidad) {
-        super(precio, capacidad);
-    }
-}
--- src/CarpaSombrilla.java
+++
@@ -1,5 +0,0 @@
-public class CarpaSombrilla extends Carpa{
-    public CarpaSombrilla(int precio, int capacidad) {
-        super(precio, capacidad);
-    }
-}
